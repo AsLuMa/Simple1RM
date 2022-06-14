d0c009be07894c2bf7b27ed2f593647693234486
asluma <ase.mattson@gmail.com>
2022-06-14 23:04:29 +0200

    Added RecyclerView. Need to put it in a grid, put the grid in the right location, and add logic to each individual grid.

--- app/src/main/java/com/example/a1rmsimple/MainActivity.java
+++ app/src/main/java/com/example/a1rmsimple/MainActivity.java
@@ -5,12 +5,21 @@ import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView calculation;
     TextView percent;
     EditText one_rm;
+
+    //TODO should this be the gridview? "Data to display"
+    private final LinkedList<String> mWeightList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private RecycleViewAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +28,28 @@ public class MainActivity extends AppCompatActivity {
         calculation = findViewById(R.id.rep_weight);
         one_rm = findViewById(R.id.enter_max);
         percent = findViewById(R.id.rep_percent);
+
+        for (int i = 0; i < 10; i++){
+            mWeightList.addLast("1" + i);
+        }
+
+        mRecyclerView = findViewById(R.id.recyclerWeight);
+        mAdapter = new RecycleViewAdapter(this, mWeightList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 
-    public void doBasicMath(View view){
-        int input = Integer.parseInt(one_rm.getText().toString());
-        String math = Integer.toString((input*70)/100);
-        calculation.setText(math);
-        System.out.println("We did basic math " + math);
+    public int doBasicMath(int percentageOfMax, int maxWeight){
+        System.out.println("Behold! We did basic math.");
+        return (maxWeight * percentageOfMax)/100;
+    }
 
+        public void updateWeightEditText(View view){
+        //TODO need to pass percentageOfMax from somewhere else
+        int calculated = doBasicMath(70, Integer.parseInt(one_rm.getText().toString()));
+        calculation.setText(String.valueOf(calculated));
+        System.out.println("We showed basic math!");
     }
 
 }
@@ -34,8 +57,8 @@ public class MainActivity extends AppCompatActivity {
 /* TODO
 
 UI:
-- sentrer edittext
-- sentrer prosenten til boksen under når den utvider seg med tre tal
+- må lage xml-fil til gridview
+- recycleview? read tutorial for how to implement
 
 Logikk:
 - separate the math from the good stuff
---
+++ app/src/main/java/com/example/a1rmsimple/RecycleViewAdapter.java
@@ -0,0 +1,53 @@
+package com.example.a1rmsimple;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.RecycleViewHolder>{
+
+    private final LinkedList<String> mWeightList;
+    private LayoutInflater mInflater;
+
+    public RecycleViewAdapter(Context context, LinkedList<String> weightList){
+        mInflater = LayoutInflater.from(context);
+        this.mWeightList = weightList;
+
+    }
+
+    class RecycleViewHolder extends RecyclerView.ViewHolder{
+        public final TextView weightTextView;
+        final RecycleViewAdapter mAdapter;
+
+        public RecycleViewHolder(View myView, RecycleViewAdapter adapter){
+            super(myView);
+            weightTextView = myView.findViewById(R.id.rep_weight);
+            this.mAdapter = adapter;
+        }
+
+    }
+
+    @NonNull
+    @Override
+    public RecycleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mView = mInflater.inflate(R.layout.grid_layout, parent, false);
+        return new RecycleViewHolder(mView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecycleViewHolder holder, int position) {
+        String mCurrent = mWeightList.get(position);
+        holder.weightTextView.setText(mCurrent);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWeightList.size();
+    }
+}
